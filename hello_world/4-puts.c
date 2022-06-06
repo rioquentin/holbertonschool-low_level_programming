c482fe9d24872c6d705bb327f8c8b576da558fea
quentin <quentin.rio.pro@gmail.com>
2022-06-06 04:00:01 -0700

    4-puts.c_fix4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    char str1[] = "\"Programming is like building a multilingual puzzle";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 
-    puts(str1);
+	puts(str1);
 
-    return (0);
+	return (0);
 }
