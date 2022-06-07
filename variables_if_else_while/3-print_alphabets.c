0adba747bf857de351f82ce9176b2ec47c7a002f
quentin <quentin.rio.pro@gmail.com>
2022-06-07 07:32:10 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char abc;
+	char ABC;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		putchar(abc);
+	}
+
+	for (ABC = 'A'; ABC <= 'Z'; ABC++)
+	{
+		putchar(ABC);
+	}
+
+
+	putchar ('\n');
+
+	return (0);
+}
