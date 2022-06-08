50e40ad461387069ee960ef41ae4db926353f600
quentin <quentin.rio.pro@gmail.com>
2022-06-08 05:46:22 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a >= 9; a++)
+		putchar(a);
+	for (b = 'a'; b >= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}	
