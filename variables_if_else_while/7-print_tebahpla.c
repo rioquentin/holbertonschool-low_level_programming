4d5eb6420ec6e4bb783cc8cad272c9dfe9404fad
quentin <quentin.rio.pro@gmail.com>
2022-06-08 02:53:31 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 9;
+
+	while (n != -1)
+	{
+		printf("%d", n);
+		n--;
+	}
+	printf("\n");
+	return (0);
+}
