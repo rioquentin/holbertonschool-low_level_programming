3dd18d8b24cca37282d0ca266c79ed644b8a702f
quentin <quentin.rio.pro@gmail.com>
2022-06-08 02:44:06 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x != 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
