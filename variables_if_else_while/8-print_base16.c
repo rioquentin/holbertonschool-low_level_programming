49284fbeb8722f4c75e4c4985933dc1fc74027cd
quentin <quentin.rio.pro@gmail.com>
2022-06-08 05:58:22 -0700

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	int a;
-	char b;
+	int x = 48;
+	char abc;
 
-	for (a = 0; a >= 9; a++)
-		putchar(a);
-	for (b = 'a'; b >= 'f'; b++)
-		putchar(b);
+	while (x != 58)
+	{
+		putchar(x);
+		x++;
+	}
+
+	for (abc = 'a'; abc <= 'f'; abc++)
+	{
+			putchar(abc);
+	}
 	putchar('\n');
 	return (0);
-}	
+}
