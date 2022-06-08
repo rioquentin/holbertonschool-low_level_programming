7b80425aea776c8dbff8aa7d56542e82eb66528a
quentin <quentin.rio.pro@gmail.com>
2022-06-08 01:26:14 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
