6606cccb11ce4a4de7e3e60cf70162a271870b5d
quentin <quentin.rio.pro@gmail.com>
2022-06-09 09:58:39 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if lower case
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+
+	else if (c >= 'A' && c <= 'Z')
+	
+		return (1);
+
+	else
+
+		return (0);
+}
