dada5aee0ca3a96d43772cccdf303ff82bea6f8f
quentin <quentin.rio.pro@gmail.com>
2022-06-09 11:10:13 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - print he absolute value.
+ * @int: number
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+
+	if (n >= 0)
+	{
+		_putchar('n');
+	}
+	else
+	{
+		n = n * -1;
+		putchar('n');
+	}
+	return (0);
+}
