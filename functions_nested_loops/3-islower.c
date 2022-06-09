fdbc9323955e27502d5884b9aece59039c503427
quentin <quentin.rio.pro@gmail.com>
2022-06-09 09:50:49 -0700

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if lower case
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+	else
+		return (0);
+}
