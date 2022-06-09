1ef1f6e719cc2da6c1fd92098a238798cb681dfb
quentin <quentin.rio.pro@gmail.com>
2022-06-09 10:20:51 -0700

    5-sign.c_fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -18,7 +18,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n > 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
