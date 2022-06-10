d04bc75366caa495fc93ecc2fc00a78fc31a0ee8
quentin <quentin.rio.pro@gmail.com>
2022-06-10 06:26:40 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,15 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		if (n < 98)
+		if (n > 98)
 		{
 			printf("%d, ", n);
-			n++;
+			n--;
 		}
 		else
 		{
 			printf("%d, ", n);
-			n--;
+			n++;
 		}
 	}
 	printf("98\n");
