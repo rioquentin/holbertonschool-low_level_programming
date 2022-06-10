b73c17db0a4bf95d0bdd0fc2c1715a0c20fccfd4
quentin <quentin.rio.pro@gmail.com>
2022-06-10 04:49:29 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - cloak
+ *
+ * return 0
+ */
+
+void jack_bauer(void)
+{
+	int h = 0;
+	int m;
+
+	while (h <= 23)
+	{
+		m = 0;
+
+		while (m <= 59)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
