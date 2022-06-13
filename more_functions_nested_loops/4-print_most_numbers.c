e9ede108dfbbf6cbfe61e31f4cee847fd0cad50f
quentin <quentin.rio.pro@gmail.com>
2022-06-13 02:57:14 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all numbers except 2 and 4
+ * Return: void.
+ */
+
+void print_most_numbers(void)
+{
+	for (i = 47; i != 58; i++)
+	{
+		if (i == 50 || i == 52)
+
+		else
+
+			_putchar(i);
+	}
+}
