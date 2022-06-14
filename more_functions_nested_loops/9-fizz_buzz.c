99115f1100a3546ade161aad3b698c4067b422d0
quentin <quentin.rio.pro@gmail.com>
2022-06-14 05:50:25 -0700

    fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,28 @@
 
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d", i);
+
+	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
