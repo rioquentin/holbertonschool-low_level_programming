472ae4ed8b79162cd1450b871242b6839555afe1
quentin <quentin.rio.pro@gmail.com>
2022-06-15 10:32:48 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - function
+ * @
+ * Return: 
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i == 0)
+		{
+			_putchar(str[i]);
+		}
+		else if (i % 2 ==0)
+		{
+			_putchar(str[i];
+					}
+					}
+					_putchar('\n');
+					}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -57,3 +57,5 @@ void _puts(char *str);
 void print_rev(char *s);
 
 void rev_string(char *s);
+
+void puts2(char *str);
