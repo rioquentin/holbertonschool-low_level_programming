14ba9ec220c563ee52c495ea459bee8c15824d1e
quentin <quentin.rio.pro@gmail.com>
2022-06-15 03:17:52 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ * @s: string
+ * Return: x
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (i; i <= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
