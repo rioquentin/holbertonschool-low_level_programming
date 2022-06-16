b76933eb8d614a1a8141d1647756574e43bc3af9
quentin <quentin.rio.pro@gmail.com>
2022-06-16 01:37:24 -0700

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,9 @@ char *_strcpy(char *dest, char *src)
 	int i;
 	int x;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] == '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	x = i + 1;
-	dest[x] = '\0';
 	return (dest);
 }
