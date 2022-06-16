b71aa59294f16dd8467c8b66286eab067101730b
quentin <quentin.rio.pro@gmail.com>
2022-06-16 10:58:15 -0700

    fix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, b, x;
 
-	/* Compteur pour connaitre le nombre de character contenu dans ce que pointe *dest */
-
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 	x = i + n;
-
-	/* Compteur pour faire augmenter la valeur dans l'array "dest" et appends le nombre de character (n) correspondant provenant de l'array src */
 
 	for (b = 0; i != x; b++, i++)
 	{
