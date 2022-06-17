0f64c1782e5d49d03d7eb68b6e127c02d204f521
quentin <quentin.rio.pro@gmail.com>
2022-06-17 14:08:16 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+nclude "holberton.h"
+
+/**
+ * cap_string - function
+ * @n: n
+ * Return: n
+ */
+
+char *cap_string(char *n)
+{
+	int i, x;
+	int z = 32;
+	int y[] = {',', ';', '.', '?', '"',
+		'(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - z;
+		}
+
+		z = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == y[x])
+			{
+				x = 12;
+				z = 32;
+			}
+		}
+	}
+	return (n);
+}
