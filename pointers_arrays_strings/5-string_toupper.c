4ec02e12fe6c81228e4e0600e9ad178a0381b496
quentin <quentin.rio.pro@gmail.com>
2022-06-17 11:02:42 -0700

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function
+ * @n: number
+ * Return: x
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (n);
+}
