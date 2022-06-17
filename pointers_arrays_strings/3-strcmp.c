ca0a7d984438bf93cb58b0caf7ec08fc5aaa3dd4
quentin <quentin.rio.pro@gmail.com>
2022-06-17 06:29:13 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - function
+ * @s1: 1
+ * @s2: 2
+ * Return: x
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
