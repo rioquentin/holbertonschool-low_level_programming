10a2b6369e01f7eebf3b3269eac26461d22fc5ea
quentin <quentin.rio.pro@gmail.com>
2022-06-20 05:52:05 -0700

    fix

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,16 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i - 1] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-	}
-	if (s[i] == '\0')
-	{
-		return (&s[i]);
 	}
 	return (NULL);
 }
