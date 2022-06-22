9abd10ce8472744c48eed701c0f92190419d21cd
quentin <quentin.rio.pro@gmail.com>
2022-06-22 05:43:12 -0700

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function
+ * @s: string
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
