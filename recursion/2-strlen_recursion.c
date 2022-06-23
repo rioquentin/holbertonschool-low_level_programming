e3b33aa819a7d7eb1c3ab7b6e4ab174891a37d6f
quentin <quentin.rio.pro@gmail.com>
2022-06-23 05:41:59 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strlen_recursion - function
@@ -8,9 +9,18 @@
 
 int _strlen_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (s[0] == '\0')
 	{
 		return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
+}
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
 }
