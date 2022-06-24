5f65d2a9cf5e331e005758414ab0e101cd19cd65
quentin <quentin.rio.pro@gmail.com>
2022-06-24 09:57:26 -0700

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	unsigned int result = num1 * num2;
 
 	(void)argc;
 	printf("%d\n", result);
