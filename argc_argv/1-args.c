20a44dd4515bc8e58197c4080273f02e7fb7b62e
quentin <quentin.rio.pro@gmail.com>
2022-06-24 07:29:15 -0700

    fix

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,7 +9,13 @@
 
 int main(int argc, char *argv[])
 {
+	int i = 1;
+	int n = argc - 1;
+	
 	(void)argv;
-	printf("%d\n", argc);
+	if (i == argc)
+		printf("0\n");
+	else
+		printf("%d\n", n);
 	return (0);
 }
