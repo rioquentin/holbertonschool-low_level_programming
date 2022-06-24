7fc02886ac62d4e73a2317e73004e67846f4840a
quentin <quentin.rio.pro@gmail.com>
2022-06-24 05:22:13 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argc
+ * @argv: argv
+ * Return: x
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%d\n", argc);
+	return (0);
+}
