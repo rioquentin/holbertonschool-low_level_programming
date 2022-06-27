c868f0c3df46bf652f6dd7ff6841e8e9d3e28919
quentin <quentin.rio.pro@gmail.com>
2022-06-27 13:01:58 -0700

    fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,10 +10,10 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, n, m, x, z;
+	int i, n, m, x;
 	char *a;
 
-	if (s1 = NULL || s2 = NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
