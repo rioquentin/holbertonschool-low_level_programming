2d3e13597942019f5b558e2b76306ec31f330895
quentin <quentin.rio.pro@gmail.com>
2022-06-27 05:31:26 -0700

    fix

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,11 +13,17 @@ char *_strdup(char *str)
 	int i, size;
 	char *a;
 
+	if (str == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	size = i;
 
-	a = malloc(size * sizeof(char));
+	a = malloc((size * sizeof(char) + 1));
 	if (a == NULL)
 		return (NULL);
 
