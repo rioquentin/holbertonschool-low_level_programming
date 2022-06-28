d4ecae79043be646e08d6e318eec38eb49c152d3
quentin <quentin.rio.pro@gmail.com>
2022-06-28 13:56:36 -0700

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function
+ * @b: int
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b * sizeof(unsigned int));
+}
