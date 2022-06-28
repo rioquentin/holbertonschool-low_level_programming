f2a314125cada440706e252b58e266de8bcbc4ed
quentin <quentin.rio.pro@gmail.com>
2022-06-28 14:24:40 -0700

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * string_nconcat - function
+ * @s1: s1
+ * @s2: s2
+ * @n: n
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, n, m, x;
+	char *a;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	a = malloc(((i + n) * sizeof(char)) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (m = 0; m < i; m++)
+	{
+		a[m] = s1[m];
+	}
+	for (x = 0; x < n; x++, m++)
+	{
+		a[m] = s2[x];
+	}
+
+	return (a);
+}
