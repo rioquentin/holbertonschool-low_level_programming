5d6c5daa088f453b5e589fe250c3631183cc22dd
quentin <quentin.rio.pro@gmail.com>
2022-06-28 09:37:29 -0700

    fix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,9 +18,13 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	a = (int **) malloc(width * sizeof(int *));
+	if (a == NULL)
+		return (NULL);
 
 	for (i = 0; i < width; i++)
 		a[i] = (int *) malloc(height * sizeof(int *));
+		if (a[i] == NULL)
+			return (NULL);
 
 	for (n = 0; n < width; n++)
 	{
