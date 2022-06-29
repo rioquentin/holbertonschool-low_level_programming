3a1b8b2822c94385df2bf16d20d0de19e17e3dd1
quentin <quentin.rio.pro@gmail.com>
2022-06-29 02:10:35 -0700

    fix

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - function
+ * @nmemb: n
+ * @size: size
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = calloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	return (p);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -108,4 +108,6 @@ void free_grid(int **grid, int height);
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
