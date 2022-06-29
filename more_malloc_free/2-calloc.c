d1fe7161ef116ddebb98ce5c5dfa9ef9ded73497
quentin <quentin.rio.pro@gmail.com>
2022-06-29 02:15:49 -0700

    fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = calloc(nmemb * size);
+	p = calloc(nmemb, size);
 	if (p == NULL)
 		return (NULL);
 
