d400a97c3e90775ec5aa597b7c3475a12b2ce990
quentin <quentin.rio.pro@gmail.com>
2022-06-29 03:15:37 -0700

    fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	if (min == max)
-		max = max + 1
+		max = max + 1;
 	p = malloc(((max - min) * sizeof(int)) + 4);
 	if (p == NULL)
 		return (NULL);
