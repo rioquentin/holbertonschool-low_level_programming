ca3feaafd01b8acd665ea84fc97d795c353a3fff
quentin <quentin.rio.pro@gmail.com>
2022-06-29 01:45:10 -0700

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,9 +23,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (x = 0;x < n; x++)
+	for (x = 0; x < n; x++)
 		;
-	size = i + x + 1;
+	size = i + x + 6;
 
 	p = malloc(size * sizeof(char));
 	if (p == NULL)
