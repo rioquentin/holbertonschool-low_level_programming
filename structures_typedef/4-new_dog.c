6d3b8863c210fba8a8a1caece1d6fca93b6d429c
quentin <quentin.rio.pro@gmail.com>
2022-07-01 11:13:43 -0700

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * new_dog - function
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: x
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogg;
+	int i, n, x;
+
+	dogg = malloc(sizeof(dog_t));
+	if (dogg == NULL)
+		return (NULL);
+
+	i = strlen(name) + 1;
+	n = strlen(owner) + 1;
+
+	dogg->name = malloc(sizeof(char) * i);
+
+	if ((*dogg).name == NULL)
+	{
+		free(dogg);
+		return (NULL);
+	}
+
+	for (x = 0; x < i; x++)
+	{
+		dogg.name[x] = name[x];
+	}
+
+	dogg->age = age;
+	dogg->owner = malloc(sizeof(char) * n);
+
+	if ((*dogg).owner == NULL)
+	{
+		free((dogg->name);
+		free(dogg);
+		return (NULL);
+	}
+	for (x = 0; x < n; x++)
+	{
+		dogg->owner[x] = owner[x];
+	}
+	return (dogg);
+}
