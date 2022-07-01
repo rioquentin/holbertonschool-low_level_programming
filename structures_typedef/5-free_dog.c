82eaf1d03b8f267fbc296014e04d42c6d49fa22c
quentin <quentin.rio.pro@gmail.com>
2022-07-01 11:21:12 -0700

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - function
+ * @d: adress
+ * Return: void
+ */
+
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(dogg->owner);
+		free(dogg->name);
+		free(dogg);
+	}
+}
