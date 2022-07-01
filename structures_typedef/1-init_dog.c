eca0efd7764c415ed5d9b4baed28c60fcf48c27e
quentin <quentin.rio.pro@gmail.com>
2022-07-01 05:30:11 -0700

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	d->name = *name;
 	d->age = age;
-	d->owner = owner;
-	return (d);
+	d->owner = *owner;
+	return (0);
 }
