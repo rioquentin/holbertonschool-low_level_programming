064bc1e42bfd22d028f276f31bd32c5508b8c100
quentin <quentin.rio.pro@gmail.com>
2022-07-01 06:02:52 -0700

    fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,11 +12,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if ((*d).name == NULL)
-		d->name = (nil);
-	if ((*d).age == NULL)
-		d->age = (nil);
+		d->name = "(nil)";
 	if ((*d).owner == NULL)
-		d->owner = (nil);
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
