dc7657939e97880b35f727f9883f40c2544a1a23
quentin <quentin.rio.pro@gmail.com>
2022-07-01 05:37:34 -0700

    fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - function
@@ -11,6 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		exit();
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
