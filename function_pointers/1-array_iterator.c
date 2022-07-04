91dcc534f89b65e962fa60ac2b09ee15e9f7ddc3
quentin <quentin.rio.pro@gmail.com>
2022-07-04 04:53:58 -0700

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator  - function
+ * @array: array
+ * @size: size
+ * @action: pointers to function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
