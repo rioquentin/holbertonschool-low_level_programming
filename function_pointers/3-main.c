963470bd0e6841b3ab57f1fc50d53345fb984d8a
quentin <quentin.rio.pro@gmail.com>
2022-07-05 13:05:39 -0700

    fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,14 +13,28 @@
 int main(int argc, char *argv[])
 {
 	int result;
+	char *op;
 
-	if (argc > 4)
+	op = argv[2];
+
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	if ((*op == '%' || *op == '/') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	printf("%d\n", result);
 
