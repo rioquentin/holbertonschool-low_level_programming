7ece922168d6fe0e7cb7b0762b227c75090c885f
quentin <quentin.rio.pro@gmail.com>
2022-07-05 11:53:12 -0700

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,20 +16,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *x;
 
-
-
 	if (n == 0)
 	{
 		printf("\n");
 		return;
 	}
-
 	va_start(ap, n);
-
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(ap, char*);
-
 		if (i == (n - 1))
 		{
 			if (x == NULL)
@@ -40,19 +35,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 				printf("%s\n", x);
 		}
-
 		else if (separator == NULL)
 			printf("%s", x);
-
 		else if (x == NULL)
 		{
 			x = "(nil)";
 			printf("%s%s", x, separator);
 		}
-
 		else
 			printf("%s%s", x, separator);
 	}
-
 	va_end(ap);
 }
