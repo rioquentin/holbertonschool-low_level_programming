d2ae91cf8b50460e59b8a0831e015488fb75840e
quentin <quentin.rio.pro@gmail.com>
2022-07-05 14:36:38 -0700

    test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,13 +1,15 @@
-#include "variadic_functions.h"
+nclude "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 
 /**
- * print_all - function to print all int
- * @format: format
- * Return: void
- */
-void print_all(const char * const format, ...)
+ *  * print_all - function to print all int
+ *   * @format: format
+ *    * Return: void
+ *     */
+
+
+void print_all(const char * const format, ...);
 {
 	va_list ap;
 	int i = 0;
@@ -16,32 +18,35 @@ void print_all(const char * const format, ...)
 	char *m;
 
 	va_start(ap, format);
+
 	while (format[x] != '\0')
 		x++;
-		while ((format[i] != '\0') && (format))
+	while (format[i] != '\0')
+	{
+		if (i == (x - 1))
+			s = "";
+
+		switch (format[i])
 		{
-			if (i == (x - 1))
-				s = "";
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c%s", va_arg(ap, int), s);
-					break;
-				case 'i':
-					printf("%d%s", va_arg(ap, int), s);
-					break;
-				case 'f':
-					printf("%f%s", va_arg(ap, double), s);
-					break;
-				case 's':
-					m = va_arg(ap, char *);
-					if (m == NULL)
-						m = "(nil)";
-					printf("%s%s", m, s);
-					break;
-			}
-			i++;
+			case 'c':
+				printf("%c%s", va_arg(ap, int), s);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(ap, int), s);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(ap, double), s);
+				break;
+			case 's':
+				m = va_arg(ap, char *);
+				if (m == NULL)
+					m = "(nil)";
+				printf("%s%s", m, s);
+				break;
 		}
+		i++;
+	}
+
 	printf("\n");
 	va_end(ap);
 }
