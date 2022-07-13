10c044c3b0644bea8e4bb7be2cc74ecd9b2e1d52
quentin <quentin.rio.pro@gmail.com>
2022-07-13 01:29:23 -0700

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_list - print linked list
+ * @h: SLL
+ * Return: i
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	list_t *trav = h;
+
+	while (trav != NULL)
+	{
+		printf("%s\n", trav->str);
+		i++;
+		trav = trav->next;
+	}
+	return (i);
+}
