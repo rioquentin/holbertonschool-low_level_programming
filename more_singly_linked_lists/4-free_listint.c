759708aa171d498b7270f22d0f5ceb82754dd432
quentin <quentin.rio.pro@gmail.com>
2022-07-14 11:17:33 -0700

    4-free_listint.c

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that free a SLL
+ * @head: pointer to a SLL
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *trash;
+
+	while (head)
+	{
+		trash = head;
+		head = head->next;
+		free(trash->n);
+		free(trash);
+	}
+}
