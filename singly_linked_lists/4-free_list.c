f2dd24874952ee5be0f19710685379f0637cc476
quentin <quentin.rio.pro@gmail.com>
2022-07-14 07:14:53 -0700

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function to free
+ * @head: SLL
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	void *trash
+
+	while (head)
+	{
+		trash = head;
+		head = head->next;
+		free(trash);
+	}
+}
