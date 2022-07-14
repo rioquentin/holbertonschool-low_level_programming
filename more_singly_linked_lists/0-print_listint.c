8ead69d583b3af7ed7c71505761342efb8fd6afb
quentin <quentin.rio.pro@gmail.com>
2022-07-14 10:35:21 -0700

    0-print_listint.c

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - function to print integer inside a SLL
+ * @h: pointer to an SLL
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int nb =0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nb++;
+	}
+	return (nb);
+}
