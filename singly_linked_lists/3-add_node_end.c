4e2b8ec6da1daa5f1b8a5601f4471350cbdf1f04
quentin <quentin.rio.pro@gmail.com>
2022-07-14 04:45:28 -0700

    fix

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -33,7 +33,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	node->next = NULL;
 
 	while ((*head)->next != NULL)
-		head = (*head)->next;
+		*head = (*head)->next;
 
 	(*head)->next = node;
 
