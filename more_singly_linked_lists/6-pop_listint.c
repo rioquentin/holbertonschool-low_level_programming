37a12af676cbc59cbe2c1406644268379f03046c
quentin <quentin.rio.pro@gmail.com>
2022-07-15 06:38:32 -0700

    fixed ;

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -11,10 +11,13 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int data;
 
+	if (*head == NULL)
+		return (0);
+
 	tmp = *head;
 	tmp = tmp->next;
 	data = (*head)->n;
-	free(head);
+	free(*head);
 	*head = tmp;
 	return (data);
 }
