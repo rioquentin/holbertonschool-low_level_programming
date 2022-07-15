e8a4540f0e729ac722be81ada4d58e02eb658c00
quentin <quentin.rio.pro@gmail.com>
2022-07-15 06:34:12 -0700

    6-pop_listint.c

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that delete the head node and save its data
+ * @head: SLL
+ * Return: value of head
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int data;
+
+	tmp = *head
+	tmp = tmp->next;
+	data = (*head)->n;
+	free(head);
+	*head = tmp;
+	return (data);
+}
