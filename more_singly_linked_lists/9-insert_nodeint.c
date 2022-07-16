54693e076f81d79823fe58e7f1380e1afed4fdda
quentin <quentin.rio.pro@gmail.com>
2022-07-16 07:15:08 -0700

    new method

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,23 +12,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *node;
-	listint_t *tmp;
 
 	if (*head == NULL)
 		return (NULL);
 	
-	tmp = *head;
-	if (idx == 0)
-	{
-		node = malloc(sizeof(listint_t));
-		if (node == NULL)
-			return (NULL);
-		node->n = n;
-		node->next = tmp;
-		tmp = node;
-		return (node);
-	}
-
 	while (*head != NULL)
 	{
 		if (i == idx)
