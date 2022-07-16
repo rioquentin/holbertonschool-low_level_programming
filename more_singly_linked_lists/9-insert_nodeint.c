fd5a7a1ffc7986e12ba0ccfafc4a07aecc274e9f
quentin <quentin.rio.pro@gmail.com>
2022-07-16 07:00:55 -0700

    new method

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint - function that insert a node with a certain value
+ * insert_nodeint_at_index - function that insert a node with a certain value
  * @head: pointer to SLL
  * @idx: index
  * @n: value
@@ -10,26 +10,26 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0, index = idx - 1;
-	listint_t *tmp;
+	unsigned int i = 0;
 	listint_t *node;
 
 	if (*head == NULL)
 		return (NULL);
 
-	tmp = *head;
-
-	for (i = 0; i != index; i++)
+	while (*head != NULL)
 	{
-		if (tmp == NULL)
-			return (NULL);
-		tmp = tmp->next;
+		if (i == idx)
+		{
+			node = malloc(sizeof(listint_t));
+			if (node == NULL)
+				return (NULL);
+			node->n = n;
+			node->next = (*head)->next;
+			(*head)->next = node;
+			return (node);
+		}
+		*head = (*head)->next;
+		i++;
 	}
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
-		return (NULL);
-	node->n = n;
-	node->next = tmp->next;
-	tmp->next = node;
-	return (node);
+	return (NULL);
 }
