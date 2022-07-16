b4bedc326ec8ac2b0738b34b638a71cc7b38fd04
quentin <quentin.rio.pro@gmail.com>
2022-07-16 04:46:24 -0700

    9-insert_nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr;
 
 	tmp = *head;
+	node = malloc(sizeof(listint_t));
 
 	for (i = 0; i < idx; i++)
 	{
