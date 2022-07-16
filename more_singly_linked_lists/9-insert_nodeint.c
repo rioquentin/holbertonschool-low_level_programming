df0f5b78cab35cdf5954850c67055cc8fdc9da73
quentin <quentin.rio.pro@gmail.com>
2022-07-16 04:38:59 -0700

    9-insert_nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,15 +14,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t tmp;
 	listint_t node;
 
-	node->n = n;
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i < idx; i++)
 	{
 		if (*head == NULL)
 			return (NULL);
 		head = (*head)->next;
 	}
 
+	node->n = n;
 	tmp = *head;
 	tmp = tmp->next;
 	node->next = tmp;
