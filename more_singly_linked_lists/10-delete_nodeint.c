341e6fc84da85b4ad8fa3ff0df5edfad32832a8e
quentin <quentin.rio.pro@gmail.com>
2022-07-16 12:51:25 -0700

    10-delete_nodeint.c

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - function that delete the n th node
+ * @head: SLL
+ * @index: n th position
+ * Return: 0 or -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *previous = *head;
+	listint_t *current = *head;
+
+	if (*head == NULL)
+		return (-1);
+	else if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+	}
+	else
+	{
+		while (index != 1)
+		{
+			previous = current;
+			current = current->next;
+			index--;
+		}
+		previous->next = current->next;
+		free(current);
+	}
+	return (1);
+}
+
