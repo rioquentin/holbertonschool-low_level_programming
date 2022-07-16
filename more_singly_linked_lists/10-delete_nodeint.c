82330b2a741d4904d4ebbb30ceae9fe2fad07e9f
quentin <quentin.rio.pro@gmail.com>
2022-07-16 13:07:26 -0700

    10-delete_nodeint.c

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 		while (index != 1)
 		{
-			if (current->next == NULL)
+			if (current->next == NULL || current == NULL)
 				return (-1);
 			previous = current;
 			current = current->next;
