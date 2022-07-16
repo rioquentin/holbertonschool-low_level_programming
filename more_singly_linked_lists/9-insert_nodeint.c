b4efd3f4e25eadec3547929019cad7e14598a06e
quentin <quentin.rio.pro@gmail.com>
2022-07-16 05:08:15 -0700

    9-insert_nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,10 +19,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	tmp = *head;
 
-	while (i < idx)
+	while (i < idx - 1)
 	{
 		if (tmp != NULL)
+		{
 			tmp = tmp->next;
+			i++;
+		}
 		else
 			return (NULL);
 	}
