c1885df97c667c6f34ce7cc1efecdd4d31786c26
quentin <quentin.rio.pro@gmail.com>
2022-07-16 07:12:26 -0700

    new method

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (node == NULL)
 			return (NULL);
 		node->n = n;
-		node->next = (*head)->next;
+		node->next = *head
 		*head = node;
 		return (node);
 	}
