00fa17d86d345957eed8acf944ae5b46c5574f13
quentin <quentin.rio.pro@gmail.com>
2022-07-19 05:25:15 -0700

    fix

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - function
+ * @n: number
+ * Return: ??
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == '0' || n == '1')
+		_putchar(n + '0');
+	else
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+}
---
+++ bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - function to print character
+ * @c: character
+ * Return: &c
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
