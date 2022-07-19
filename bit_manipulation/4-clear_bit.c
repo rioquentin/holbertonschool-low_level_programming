c2fce6f2cf4dd59fdd9c26d8c33b166378047ca7
quentin <quentin.rio.pro@gmail.com>
2022-07-19 06:40:46 -0700

    4-clear_bit.c

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - function
+ * @n: value
+ * @index: index
+ * Return: 1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > ((sizeof(n) * 8)))
+		return (-1);
+	*n = ((0 << index) & *n);
+	return (1);
+}
