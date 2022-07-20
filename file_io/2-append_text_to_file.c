c0845b411d77af021b7ce163ffd0894e17ab8518
quentin <quentin.rio.pro@gmail.com>
2022-07-20 13:23:13 -0700

    2-append_text_to_file.c

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function 
+ * @filename: file name
+ * @text_text: text
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, size;
+
+	if (filename == NULL)
+		return (-1);
+	for (size = 0; text_content[size]; size++)
+		;
+
+	fd = open(filename, O_APPEND | O_RDWR);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, size);
+	close(fd);
+	return(1);
+}
