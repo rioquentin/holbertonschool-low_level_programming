8dc02bbf8187f14510357447aeceb84246b1a6bb
quentin <quentin.rio.pro@gmail.com>
2022-07-20 12:22:32 -0700

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int wr, i, fd;
+	int wr, i, fd, txt;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,8 +20,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		close(fd);
-		return (-1);
+		txt = open(text_content, O_CREAT | O_RDONLY);
+		if (txt == -1)
+			return (-1);
 	}
 
 	for (i = 0; text_content[i]; i++)
@@ -30,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	wr = write(fd, text_content, i);
 	if (wr == -1)
 		return (-1);
-
+	close(txt);
 	close(fd);
 	return (1);
 }
