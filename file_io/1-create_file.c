10a4418e2a05f0890049d42de167c6ed1a9afc5d
quentin <quentin.rio.pro@gmail.com>
2022-07-20 12:50:33 -0700

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		txt = open("test.txt", O_CREATE | O_RDONLY);
+		txt = open("test.txt", O_CREAT | O_RDONLY);
 		if (txt == -1)
 			return (-1);
 		read(txt, text_content, 0);
