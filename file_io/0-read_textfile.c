6ffc0d4673352bf3ed342b756bc37fff33d5a052
quentin <quentin.rio.pro@gmail.com>
2022-07-20 06:48:00 -0700

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - read
+ * @filename: filename
+ * @letters: letters
+ * Return: yeet
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, counter;
+	const char *buff;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	counter = read(fd, buff, letters);
+	close(fd);
+	
+	return (write(1, buff, letters)); 
+}
+
+
