6206c41ab35cc9d265b1be23a6d324e88734557f
quentin <quentin.rio.pro@gmail.com>
2022-07-21 04:26:58 -0700

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		node->n = n;
-		node->next = *head;
+		node->next = NULL;
 		node->prev =  NULL;
 		*head = node;
 	}
