1c3d82025b72dea97978592e69c5c61a45b245d5
quentin <quentin.rio.pro@gmail.com>
2022-07-21 11:07:10 -0700

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum them all
+ * @head: DLL
+ * Return: sum or 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int i, sum;
+
+	if (head == NULL)
+		return (0);
+
+	for (i = 0; head->next != NULL; i++)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum;
+}
