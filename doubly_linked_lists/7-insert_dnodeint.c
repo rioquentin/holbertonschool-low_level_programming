2c7b8f79471a73e63cb14ff5e716d6b1f17bd6b0
quentin <quentin.rio.pro@gmail.com>
2022-07-21 15:41:28 -0700

    7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - func
+ * @h: sll
+ * @idx: index
+ * @n: value
+ * Return: node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	int i = 0;
+	dlistint_t *node;
+	dlistint_t *tmp;
+
+	if (*h == NULL)
+		return (0);
+
+	node = malloc(sizeof(dlistint_t));
+	tmp = malloc(sizeof(dlistint_t));
+	tmp = *h;
+
+	while (i < idx)
+	{
+		i++;
+		tmp = *h;
+		(*h) = (*h)->next;
+		(*h)->prev = tmp;
+	}
+	node->n = n;
+	tmp->next = node;
+	node->next = *h;
+	node->prev = tmp;
+	*h = node;
+	return (node);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -27,5 +27,6 @@ size_t dlistint_len(const dlistint_t *h);
 void free_dlistint(dlistint_t *head);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 #endif
