ad839b7a5940c158e51b073a340fd0db8d4d18a6
quentin <quentin.rio.pro@gmail.com>
2022-07-21 01:44:32 -0700

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	dlistint_t *node;
 
-	node = malloc(sizeof(listint_t));
+	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 	{
 		free(node);
