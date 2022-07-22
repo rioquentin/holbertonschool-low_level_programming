609f2bd9209a6940a98db1846141e33a512911d5
quentin <quentin.rio.pro@gmail.com>
2022-07-22 06:27:53 -0700

    fxi

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		add_dnodeint(h, n);
 		return (0);
-
+	}
 	node = malloc(sizeof(dlistint_t));
 	tmp = malloc(sizeof(dlistint_t));
 	tmp = *h;
