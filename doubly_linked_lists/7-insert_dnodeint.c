ed4a309a37977a4b1eca14497b514b1c7f24ca57
quentin <quentin.rio.pro@gmail.com>
2022-07-22 06:41:34 -0700

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,8 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 	{
-		add_dnodeint(h, n)
-			return (node);
+		add_dnodeint(h, n);
+		return (node);
 	}
 	node = malloc(sizeof(dlistint_t));
 	tmp = *h;
