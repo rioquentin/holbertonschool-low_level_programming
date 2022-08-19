91a51d31a7bb90eea0143b7eebe75cef81482da7
quentin <quentin.rio.pro@gmail.com>
2022-08-19 05:27:42 -0700

    fix memory

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	htable = malloc(sizeof(hash_table_t));
 	if (htable == NULL)
 		return (NULL);
-	htable->array = malloc(sizeof(hash_node_t) * size);
+	htable->array = malloc(sizeof(hash_node_t *) * size);
 	if (htable->array == NULL)
 	{
 		free(htable);
