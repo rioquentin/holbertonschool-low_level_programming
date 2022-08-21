080a012810b4a70fb8ef2750705d1f1e1fec5ddb
quentin <quentin.rio.pro@gmail.com>
2022-08-21 14:49:36 -0700

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,7 +10,6 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *vtmp = NULL;
 	unsigned int ki = key_index((unsigned char *)key, ht->size);
 	hash_node_t *node;
 	hash_node_t *tmp;
