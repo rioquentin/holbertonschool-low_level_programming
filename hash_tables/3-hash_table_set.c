ae9ef67c139849b28de7eb66805b3239e389250b
quentin <quentin.rio.pro@gmail.com>
2022-08-21 08:15:04 -0700

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,11 +13,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char * vtmp = NULL;
 	unsigned int ki = key_index((unsigned char *)key, ht->size);
 	hash_node_t * node;
+	hash_node_t * tmp;
 
 	if (ht->array == NULL || value == NULL || ht == NULL)
 		return (0);
 
 	vtmp = strdup(value);
+
+	tmp = ht->array[ki];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = vtmp;
+			tmp->value = strdup(value);
+			free(vtmp);
+			return (1);
+		}
+		tmp = temp->next;	
+	}
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
