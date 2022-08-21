6175c397123601c9e368cd95748408040236b9c9
quentin <quentin.rio.pro@gmail.com>
2022-08-21 08:07:08 -0700

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char * vtmp = NULL;
-	unsigned int ki = key_index(*key, ht->size);
+	unsigned int ki = key_index((unsigned char *)key, ht->size);
 	hash_node_t * node;
 
 	if (ht == NULL)
