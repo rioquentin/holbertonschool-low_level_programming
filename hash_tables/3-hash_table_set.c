732b9d09d9fdb6c69134ccfc97219c559cb6ecba
quentin <quentin.rio.pro@gmail.com>
2022-08-21 14:52:06 -0700

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	hash_node_t *tmp;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL)
 		return (0);
 
 	tmp = ht->array[ki];
